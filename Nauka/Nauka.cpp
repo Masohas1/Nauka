54d1b7c304cb63895d154d2ba5e707494343de93
Masohas1 <jarekbabiak3@gmail.com>
2023-03-11 22:14:27 +0100

    Wskaźnik * i &

--- Nauka/Nauka.cpp
+++ Nauka/Nauka.cpp
@@ -4,9 +4,12 @@ using namespace std;
 
 int main()
 {
-    int* ap = NULL;
+    int jarek = 75;
+    int* ap = & jarek;
 
     cout << ap << endl;
+    cout << & ap << endl;
+    cout << *ap << endl;
 
 
     return 0;
