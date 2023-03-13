fd5b42f5c104a3b47b2a7df868ccbb860f72315f
Masohas1 <jarekbabiak3@gmail.com>
2023-03-13 22:40:28 +0100

    działanie tab , proba naprawienia tab2

--- Nauka/Nauka.cpp
+++ Nauka/Nauka.cpp
@@ -1,10 +1,21 @@
 ﻿#include <iostream>
 
+void sortuj(int* begin, int* end) {
+    for (int* i = begin; i != end; ++i) {
+        for (int* j = i+1 ; j < end ; j++) {
+            std::cout << *(j - 1)<<" " << *j << std::endl;
+            if (*(j - 1) > *j) {
+                std::swap(*(j - 1), *j);
+            }
+        }
+    }
+}
+
 void wypisz(int* begin, int size)
 {
     while (size > 0)
     {
-        std::cout << *begin << ' ';
+        //std::cout << *begin << ' ';
         ++begin;
         --size;
     }
@@ -12,9 +23,9 @@ void wypisz(int* begin, int size)
 
 int main()
 {
-    int tab[10] = { 0, 9, 1, 3, 8, 2, 6, 7, 5, 4 };
+    /*int tab[10] = { 0, 9, 1, 3, 8, 2, 6, 7, 5, 4 };
     sortuj(tab, tab + 10);
-    wypisz(tab, 10);
+    wypisz(tab, 10);*/
 
     std::cout << '\n';
 
