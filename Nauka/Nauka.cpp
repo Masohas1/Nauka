480e806e0c0cf07e08ce980dea058529736a1854
Masohas1 <jarekbabiak3@gmail.com>
2023-03-13 19:37:54 +0100

    dodanie funckji sortuj

--- Nauka/Nauka.cpp
+++ Nauka/Nauka.cpp
@@ -1,16 +1,24 @@
 ﻿#include <iostream>
 
-using namespace std;
+void wypisz(int* begin, int size)
+{
+    while (size > 0)
+    {
+        std::cout << *begin << ' ';
+        ++begin;
+        --size;
+    }
+}
 
-//void gen(int* wJarek, int number) {
-//
-//}
+int main()
+{
+    int tab[10] = { 0, 9, 1, 3, 8, 2, 6, 7, 5, 4 };
+    sortuj(tab, tab + 10);
+    wypisz(tab, 10);
 
-int main() {
-	int jarek[2] = {1,2};
-	/*gen(jarek, 1);*/
-	cout << jarek << endl;
-	cout << jarek + 10 << endl;
+    std::cout << '\n';
 
-	return 0;
+    int tab2[16] = { 9, 7, 8, 6, 5, 4, 4, 0, 9, 6, 7, 1, 6, 3, 1, -100 };
+    sortuj(tab2, tab2 + 15);
+    wypisz(tab2, 15);
 }
